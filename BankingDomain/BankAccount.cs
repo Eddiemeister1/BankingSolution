df1acac1e0e4d88af6c8c6607f52123c6e0cc915
Eduardo Dotel <eduardodotel2@gmail.com>
2021-07-28 14:00:01 -0400

    Add project files.

---
+++ BankingDomain/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BankingDomain
+{
+    public class BankAccount
+    {
+        private decimal _balance = 5000;
+
+        
+        public decimal GetBalance()
+        {
+            return _balance;
+        }
+
+        public void Withdraw(decimal amountToWithdraw)
+        {
+            _balance -= amountToWithdraw;
+
+        }
+    }
+}
---
+++ BankingTests/NewAccounts.cs
@@ -0,0 +1,26 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankingTests
+{
+    public class NewAccounts
+    {
+        [Fact]
+        public void NewAccountsHaveCorrectBalance()
+        {
+            // Given (arrange)
+            var account = new BankAccount();
+
+            //When (Act)
+            decimal openingBalance = account.GetBalance();
+
+            //Then (Assert)
+            Assert.Equal(5000M, openingBalance);
+        }
+    }
+}
