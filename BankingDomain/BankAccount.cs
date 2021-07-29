53b16b66c42feba7dcf9d8b276ec4ac09f84b4d6
Eduardo Dotel <eduardodotel2@gmail.com>
2021-07-29 11:31:59 -0400

    done with the flags things

--- BankingDomain/BankAccount.cs
+++ BankingDomain/BankAccount.cs
@@ -2,11 +2,12 @@
 
 namespace BankingDomain
 {
+    public enum AccountType { Standard, Gold}
     public class BankAccount
     {
         private decimal _balance = 5000;
 
-        
+        public AccountType AccountType { get; set; } = AccountType.Standard;
         public decimal GetBalance()
         {
             return _balance;
@@ -16,7 +17,7 @@ namespace BankingDomain
         {
             if(amountToWithdraw >= _balance)
             {
-                //throw new OverdraftNotAllowedException();
+                throw new OverdraftNotAllowedException();
             } else
             {
                 _balance -= amountToWithdraw;
@@ -26,7 +27,12 @@ namespace BankingDomain
 
         public void Deposit(decimal amountToDeposit)
         {
-            _balance += amountToDeposit;
+            var bonus = 0M;
+            if(AccountType == AccountType.Gold)
+            {
+                bonus = amountToDeposit * .10M;
+            }
+            _balance += amountToDeposit + bonus;
         }
     }
 }
---
+++ BankingTests/GoldAccountTests/GoldAccountsGetABonusOnDeposits.cs
@@ -0,0 +1,26 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankingTests.GoldAccountTests
+{
+    public class GoldAccountsGetABonusOnDeposits
+    {
+        [Fact]
+        public void TheBonusIsAppliedToTheirBalance()
+        {
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+            var amountToDeposit = 100M;
+            account.AccountType = AccountType.Gold;
+
+            account.Deposit(amountToDeposit);
+
+            Assert.Equal(amountToDeposit + 10 + openingBalance, account.GetBalance());
+        }
+    }
+}
