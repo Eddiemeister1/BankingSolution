50b0861cefbe95a9c0526f1d68c9039fbbb6c870
Eduardo Dotel <eduardodotel2@gmail.com>
2021-07-29 11:57:18 -0400

    Inheritance completed

---
+++ BankingDomain/PlatinumAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingDomain
+{
+    public class PlatinumAccount : BankAccount
+    {
+        public override void Deposit(decimal amountToDeposit)
+        {
+            base.Deposit(amountToDeposit * 1.2M);
+        }
+    }
+}
